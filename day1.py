73fe6f10e138938e94d8f4e4179deb1c15b4c256
mrbenkoe <68224595+mrbenkoe@users.noreply.github.com>
2025-04-25 17:50:55 +0200

    Day 1, Part 2

--- day1.py
+++ day1.py
@@ -15,11 +15,15 @@ data = np.array(data, dtype=int)
 
 #print(data)
 
-col_1 = np.sort(data[:, 0])
-col_2 = np.sort(data[:, 1])
+col1 = np.sort(data[:, 0])
+col2 = np.sort(data[:, 1])
 
-result = sum(abs(col_2-col_1))
+result = sum(abs(col2-col1))
 
-print("result 1 = ",result)
+print("result 1 = ", result)
 
+result2 = 0
+for val in list(col1):
+    result2 += val*list(col2).count(val)
 
+print("result 2 = ", result2)
