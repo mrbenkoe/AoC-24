579ad6d7a113d47b6cf4d20333b97abd8a9b9c75
mrbenkoe <68224595+mrbenkoe@users.noreply.github.com>
2025-06-02 15:47:09 +0200

    Day 5, Part 2

--- day5.py
+++ day5.py
@@ -14,12 +14,11 @@ with open(path, "r") as f:
         elif ',' in line:
             updates.append(list(map(int, line.split(','))))
 
-print("rules =",rules)
-print("updates =",updates)
-
-result =[]
+result = []
+incorrect = []
 for i, update in enumerate(updates):
     a = list(map(list, combinations(update, 2)))
+ 
     counter = 0
     for item in a:
         for rule in rules:
@@ -27,7 +26,22 @@ for i, update in enumerate(updates):
                 counter += 1
     if counter == len(a):  
         result.append(update[int(len(update)/2)])
+    else:
+        incorrect.append(update)
 
 print("result =", sum(result)) #5087
 
+result = 0
+for i, update in enumerate(incorrect):
+    for j in range(5):
+        for r in rules:
+            if len([x for x in update if x in r]) == 2:
+                index_0 = update.index(r[0])
+                index_1 = update.index(r[1])
+                if index_0 > index_1:
+                    update[index_0] = r[1]
+                    update[index_1] = r[0]
 
+    result += update[int(len(update)/2)]
+
+print("result =", result) #4971
